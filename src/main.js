c3d7c384b47498dffc4d30edc5ff16b01753b1fd
caohai <1106225027@qq.com>
2021-05-19 11:27:14 +0800

    入口新增element-plus

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { useElementPlus } from './plugins/element-plus';
 
-createApp(App).mount('#app')
+createApp(App)
+.use(useElementPlus)
+.mount('#app')
