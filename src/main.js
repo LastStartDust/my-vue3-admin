d5463a3956834ce7b7e62d4aeafadfceea1d71af
caohai <1106225027@qq.com>
2021-05-19 15:39:03 +0800

    配置别名

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { useElementPlus } from './plugins/element-plus';
+import { useElementPlus } from '@/plugins/element-plus';
+import store from '@/store';
 
 createApp(App)
 .use(useElementPlus)
+.use(store)
 .mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import styleImport from 'vite-plugin-style-import'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,5 +22,10 @@ export default defineConfig({
         }
       ]
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 })
