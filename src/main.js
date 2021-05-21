5f1ba57ac51140d094c5709827175c6abc48011f
caohai <1106225027@qq.com>
2021-05-21 12:47:33 +0800

    新增权限和设置文件

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { useElementPlus } from '@/plugins/element-plus';
 import store from '@/store';
+import router from '@/router';
+import '@/permission' // permission control
+import '@/styles/index.scss' // global css
 
 createApp(App)
+.use(router)
 .use(useElementPlus)
 .use(store)
 .mount('#app')
---
+++ src/settings.js
@@ -0,0 +1,27 @@
+// 是否使用mock
+export default {
+
+  title: 'my-vue3-admin',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether need tagsView
+   */
+  tagsView: true,
+  /**
+   * @type {boolean} true | false
+   * @description 是否使用mock, 只在开发环境使用
+   */
+}
