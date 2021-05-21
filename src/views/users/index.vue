c2a8bfecc466d5db506e4b4b01fae641457da683
caohai <1106225027@qq.com>
2021-05-21 12:51:44 +0800

    新增用户页面

---
+++ src/views/users/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    用户列表页面
+  </div>
+  <div>
+    <p v-if="list">{{ list }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserList',
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    getList() {
+      return new Promise((resolve, reject) => {
+        const mockList = new Array(10).fill(1).map((_, index) => index +1)
+        this.list = Object.assign([], mockList)
+        resolve(mockList)
+      })
+    }
+  }
+}
+</script>
