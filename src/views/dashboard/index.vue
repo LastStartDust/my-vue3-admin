a353f1af0a34d3c761cf18b828abdbe4b757097f
caohai <1106225027@qq.com>
2021-05-21 12:51:33 +0800

    新增首页

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="dashboard-container">
+    <div>首页</div>
+    <div>设备：{{ device }}</div>
+    <!-- <div class="dashboard-text">用户名: {{ name }}</div>
+    <div class="dashboard-text">角色列表: <span v-for="role in roles" :key="role">{{ role }}</span></div> -->
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      // 'name',
+      // 'roles',
+      'device'
+    ])
+  },
+  created() {
+
+  },
+  mounted () {
+    console.log(this.$store);
+    console.log(this.$router);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
