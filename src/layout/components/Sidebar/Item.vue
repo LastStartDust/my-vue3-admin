840cf6def6f0b61c07ba46e854471f6dbb34e4ce
caohai <1106225027@qq.com>
2021-05-21 12:49:21 +0800

    新增布局组件

---
+++ src/layout/components/Sidebar/Item.vue
@@ -0,0 +1,29 @@
+<template>
+  <i v-if="icon" class="sub-el-icon" :class="icon"></i>
+  <span v-if="title">{{ title }}</span>
+</template>
+
+<script>
+export default {
+  name: 'MenuItem',
+  functional: true,
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.sub-el-icon {
+  color: currentColor;
+  width: 1em;
+  height: 1em;
+}
+</style>
