86f188b3851d2e40853276a6941d7e5407687f1b
caohai <1106225027@qq.com>
2021-05-28 18:28:47 +0800

    注册SvgIcon组件

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import { createApp } from 'vue'
+import '@/permission' // permission control
+import '@/plugins/execute-mock-xhr'
+import '@/styles/index.scss' // global css
+
 import App from './App.vue'
 import { useElementPlus } from '@/plugins/element-plus';
+import svgIcon from '@/components/svgIcon/index.vue'
 import store from '@/store';
 import router from '@/router';
-import '@/permission' // permission control
-import '@/styles/index.scss' // global css
 
 createApp(App)
 .use(router)
 .use(useElementPlus)
 .use(store)
+.component('svg-icon', svgIcon)
 .mount('#app')
