e9f2e428c37d9362d04f6601ec24d0b2cf192f53
caohai <1106225027@qq.com>
2021-05-28 18:30:31 +0800

    更新右侧下拉菜单

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -7,25 +7,20 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <img :src="avatar" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
-        <el-dropdown-menu slot="dropdown" class="user-dropdown">
-          <router-link to="/">
-            <el-dropdown-item>
-              Home
+
+        <template #dropdown>
+          <el-dropdown-menu  class="user-dropdown">
+            <router-link to="/">
+              <el-dropdown-item>首页</el-dropdown-item>
+            </router-link>
+            <el-dropdown-item divided @click.native="logout">
+              <span style="display:block;">登 出</span>
             </el-dropdown-item>
-          </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
-            <el-dropdown-item>Github</el-dropdown-item>
-          </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
-          </a>
-          <el-dropdown-item divided @click.native="logout">
-            <span style="display:block;">Log Out</span>
-          </el-dropdown-item>
-        </el-dropdown-menu>
+          </el-dropdown-menu>
+        </template>
       </el-dropdown>
     </div>
   </div>
@@ -35,8 +30,14 @@
 import { mapGetters } from 'vuex'
 import Breadcrumb from '@/components/Breadcrumb/index.vue'
 import Hamburger from '@/components/Hamburger/index.vue'
+import avatar from '@/assets/avatar.gif';
 
 export default {
+  data() {
+    return {
+      avatar
+    }
+  },
   components: {
     Breadcrumb,
     Hamburger
@@ -44,7 +45,7 @@ export default {
   computed: {
     ...mapGetters([
       'sidebar',
-      'avatar'
+      // 'avatar'
     ])
   },
   methods: {
@@ -111,7 +112,7 @@ export default {
       }
     }
 
-    .avatar-container {
+    :deep .avatar-container {
       margin-right: 30px;
 
       .avatar-wrapper {
