b98161f0caacbb85413c8ea9eff7a5595fed8fd7
caohai <1106225027@qq.com>
2021-06-01 15:32:41 +0800

    修改嵌套组件router-view警告

--- src/views/nested/menu1/index.vue
+++ src/views/nested/menu1/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div style="padding:30px;">
-    <el-alert :closable="false" title="menu 1">
-      <router-view />
-    </el-alert>
+  <div style="padding: 30px">
+    <router-view v-slot="{ Component }">
+      <transition>
+        <el-alert :closable="false" title="menu 1">
+          <component :is="Component" />
+        </el-alert>
+      </transition>
+    </router-view>
   </div>
 </template>
--- src/views/nested/menu1/menu1-1/index.vue
+++ src/views/nested/menu1/menu1-1/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div style="padding:30px;">
-    <el-alert :closable="false" title="menu 1-1" type="success">
-      <router-view />
-    </el-alert>
+  <div style="padding: 30px">
+    <router-view v-slot="{ Component }">
+      <transition>
+        <el-alert :closable="false" title="menu 1-1" type="success">
+          <component :is="Component" />
+        </el-alert>
+      </transition>
+    </router-view>
   </div>
 </template>
