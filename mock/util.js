41c7872faceac4dbbc822acbe6dcf5d459fbf531
caohai <1106225027@qq.com>
2021-06-02 19:15:10 +0800

    新增解析JSON，解析query和解析原始请求的函数

--- mock/util.js
+++ mock/util.js
@@ -1,3 +1,6 @@
+import url from 'url'
+import { match } from 'path-to-regexp';
+
 /**
  * @param {string} url
  * @returns {Object}
@@ -58,3 +61,59 @@ export function getArrRandomCount(arr, len) {
 export const SUCCESS_CODE = 0
 export const ERROR_CODE = -1
 
+// 解析JSON
+export function parseJson(req) {
+  return new Promise((resolve) => {
+    let body = "";
+    let jsonStr = "";
+    req.on("data", function(chunk) {
+      body += chunk;
+    });
+    req.on("end", function() {
+      try {
+        jsonStr = JSON.parse(body);
+      } catch (err) {
+        jsonStr = "";
+      }
+      resolve(jsonStr);
+      return;
+    });
+  });
+}
+
+// 解析Query
+export function parseQuery(req) {
+  return new Promise((resolve) => {
+    let queryParams = {};
+    const isGet = req.method && req.method.toUpperCase() === "GET";
+    if (isGet && req.url) {
+      queryParams = url.parse(req.url, true)
+    }
+    const reqUrl = isGet ? queryParams.pathname : req.url
+    const urlMatch = match(url, { decode: decodeURIComponent });
+
+    let query = queryParams.query
+    if (reqUrl) {
+      if (isGet && JSON.stringify(query) === "{}" || !isGet) {
+        query = urlMatch(reqUrl).params || {};
+      }
+    }
+    resolve(query)
+    return
+  })
+}
+
+// 解析原始请求
+export function parseRawRequest(req) {
+  return new Promise(async (resolve) => {
+    const body = await parseJson(req)
+    const query = await parseQuery(req)
+    resolve({
+      url: req.url,
+      body,
+      query,
+      headers: req.headers
+    })
+    return 
+  })
+}
