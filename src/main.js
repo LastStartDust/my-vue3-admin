bc4169427126690c511e20ea6c7591aa49e6aeb1
caohai <1106225027@qq.com>
2021-07-07 15:32:04 +0800

    格式化main.js

--- src/main.js
+++ src/main.js
@@ -3,16 +3,16 @@ import '@/permission' // permission control
 import '@/styles/index.scss' // global css
 
 import App from './App.vue'
-import { useElementPlus } from '@/plugins/element-plus';
+import { useElementPlus } from '@/plugins/element-plus'
 import svgIcon from '@/components/svgIcon/index.vue'
-import store from '@/store';
-import router from '@/router';
+import store from '@/store'
+import router from '@/router'
 import Pagination from '@/components/Pagination/index.vue'
 
 createApp(App)
-.use(router)
-.use(useElementPlus)
-.use(store)
-.component('svg-icon', svgIcon)
-.component('Pagination', Pagination)
-.mount('#app')
+  .use(router)
+  .use(useElementPlus)
+  .use(store)
+  .component('svg-icon', svgIcon)
+  .component('Pagination', Pagination)
+  .mount('#app')
